f916ab71643ae3012bd40e9f98e537a4cdc8a817
Mridul Gupta <mgvit2021@gmail.com>
2020-05-10 18:22:08 +0530

    added-heroku-server

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
-module.exports = new Sequelize('greenfield', 'postgres', '1259', {
-    host: 'localhost',
+require('dotenv').config();
+module.exports = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres'
 });
