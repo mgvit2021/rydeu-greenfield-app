29d5ee3dd72e0e6d104a5ba98e5c2b8d7ab82b0b
Mridul Gupta <mgvit2021@gmail.com>
2020-05-10 20:21:59 +0530

    fixed-error

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-module.exports = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+module.exports = new Sequelize(process.env.DB_DATABASE,process.env.DB_USER,process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_USER
 });
