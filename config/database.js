65b804b27318119755ee163838d8348c3d33ffa7
Mridul Gupta <mgvit2021@gmail.com>
2020-05-10 18:35:57 +0530

    minor-fix

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 module.exports = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: process.env.DB_USER
 });
